5a0e49ac587fe3d030a8d426f4e2b36746defd56
Pri <pillaypriya998@gmail.com>
2025-06-21 20:12:01 +0200

    update

--- hideAndShow.js
+++ hideAndShow.js
@@ -5,6 +5,18 @@ const tableContainer4 = document.querySelector('#sept25');
 const tableContainer5 = document.querySelector('#oct25');
 const tableContainer6 = document.querySelector('#nov25');
 const tableContainer7 = document.querySelector('#dec25');
+const tableContainer8 = document.querySelector('#jan26');
+const tableContainer9 = document.querySelector('#feb26');
+const tableContainer10 = document.querySelector('#mar26');
+const tableContainer11 = document.querySelector('#apr26');
+const tableContainer12 = document.querySelector('#may26');
+const tableContainer13 = document.querySelector('#jun26');
+const tableContainer14 = document.querySelector('#jul26');
+const tableContainer15 = document.querySelector('#aug26');
+const tableContainer16 = document.querySelector('#sep26');
+const tableContainer17 = document.querySelector('#oct26');
+const tableContainer18 = document.querySelector('#nov26');
+const tableContainer19 = document.querySelector('#dec26');
 let isClicked1 = true;
 let isClicked2 = true;
 let isClicked3 = true;
@@ -12,6 +24,18 @@ let isClicked4 = true;
 let isClicked5 = true;
 let isClicked6 = true;
 let isClicked7 = true;
+let isClicked8 = true;
+let isClicked9 = true;
+let isClicked10 = true;
+let isClicked11 = true;
+let isClicked12 = true;
+let isClicked13 = true;
+let isClicked14 = true;
+let isClicked15 = true;
+let isClicked16 = true;
+let isClicked17 = true;
+let isClicked18 = true;
+let isClicked19 = true;
 
 let showAndHide = function(table){
     if(table == tableContainer1){
@@ -75,6 +99,114 @@ let showAndHide = function(table){
             }else{
                 table.style.display = 'none';
                 isClicked7 = true
+            }
+     }else 
+        if(table === tableContainer8){
+            if(isClicked8){
+                table.style.display = 'table';
+                isClicked8 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked8 = true
+            }
+    }else 
+        if(table === tableContainer9){
+            if(isClicked9){
+                table.style.display = 'table';
+                isClicked9 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked9 = true
+            }
+    }else 
+        if(table === tableContainer10){
+            if(isClicked10){
+                table.style.display = 'table';
+                isClicked10 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked10 = true
+            }
+    }else 
+        if(table === tableContainer11){
+            if(isClicked11){
+                table.style.display = 'table';
+                isClicked11 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked11 = true
+            }
+    }else 
+        if(table === tableContainer12){
+            if(isClicked12){
+                table.style.display = 'table';
+                isClicked12 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked12 = true
+            }
+    }else 
+        if(table === tableContainer13){
+            if(isClicked13){
+                table.style.display = 'table';
+                isClicked13 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked13 = true
+            }
+    }else 
+        if(table === tableContainer14){
+            if(isClicked14){
+                table.style.display = 'table';
+                isClicked14 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked14 = true
+            }
+    }else 
+        if(table === tableContainer15){
+            if(isClicked15){
+                table.style.display = 'table';
+                isClicked15 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked15 = true
+            }
+    }else 
+        if(table === tableContainer16){
+            if(isClicked16){
+                table.style.display = 'table';
+                isClicked16 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked16 = true
+            }
+    }else 
+        if(table === tableContainer17){
+            if(isClicked17){
+                table.style.display = 'table';
+                isClicked17 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked17 = true
+            }
+    }else 
+        if(table === tableContainer18){
+            if(isClicked18){
+                table.style.display = 'table';
+                isClicked18 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked18 = true
+            }
+    }else 
+        if(table === tableContainer19){
+            if(isClicked19){
+                table.style.display = 'table';
+                isClicked19 = false;
+            }else{
+                table.style.display = 'none';
+                isClicked19 = true
             }
     }  
 }     
